47b05fada8ea87ced8f337b6e8996f9e9146e84b
iwonakiejno <iwonakiejno@gmail.com>
2020-07-12 10:55:24 +0200

    Zadanie4

--- src/main/java/Zadania_java_sda/Zad3.java
+++ src/main/java/Zadania_java_sda/Zad3.java
@@ -2,6 +2,18 @@ package Zadania_java_sda;
 
 public class Zad3 {
     // Napisz funkcję, która dla zadanych dwóch parametrów X i Y obliczy sumę liczb od X do Y
+    public static int sumOfArray(int x, int y) {
+        int sum = 0;
+        for (int i = x; i <= y; i++){
+            sum += i;
 
-    
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int sumXY = sumOfArray(1,4);
+        System.out.println(sumXY);
+
+    }
 }
---
+++ src/main/java/Zadania_java_sda/Zad4.java
@@ -0,0 +1,32 @@
+package Zadania_java_sda;
+
+public class Zad4 {
+    // Dla zadanej tablicy 3 intów zamień miejscami pierwszy i trzeci element.
+    //Przykład:
+    // + zadana tablica [12, 7, 19]
+    //wynik [19, 7, 12]
+
+    public static int[] switchTable(int[] array1) {
+        if (array1.length > 1){
+            int tmp = array1[0];
+            array1[0] = array1[array1.length -1];
+            array1[array1.length -1] = tmp;
+        }
+
+        return array1;
+    }
+    public static void printArray (int[] array) {
+        for ( int value : array ) {
+            System.out.print("[" + value + "] ");
+
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String[] args) {
+        int[] beforeArray = {12, 7, 19};
+        printArray(beforeArray);
+        int[] afterArray = switchTable(beforeArray);
+        Zad4.printArray(afterArray);
+    }
+}
